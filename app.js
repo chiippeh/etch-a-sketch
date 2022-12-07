db6b7107ca321f1cf4f10647ade7edba9492ddb8
Manu <jourdan.manu@gmail.com>
2022-12-07 09:01:00 +0200

    Add grid hover and button click event functionality

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const gridContainerDiv = document.querySelector('.grid-container');
+const squareNumButton = document.querySelector('.num-squares');
+const squareNumText = document.querySelector('.num-squares-text');
 let num = 16;
 let gridDimension = ((0.85 * document.documentElement.clientHeight) / num); 
-console.log(gridDimension);
 
+console.log(gridContainerDiv);
 
-// const div = document.createElement('div');
-// div.style.cssText = `display: flex; width: ${gridDimension}px; height: ${gridDimension}px; background-color: red;`
 for (let i = 1; i <= num*num; i++) {
     let div = gridContainerDiv.appendChild(document.createElement('div'))
     div.className = `g${i}`
     div.style.cssText = `width: ${gridDimension}px;
-    height: ${gridDimension}px; background-color: red;
-    box-shadow: inset 0 0 1px #000;`
-    div.classList.add('grid-block:hover')
-
-
-    //35.5938
+    height: ${gridDimension}px; background-color: black;`
+    //box-shadow: inset 0 0 5px #000F;
+    div.classList.add('block-element');
 }
+
+const blocks = document.querySelectorAll('.block-element');
+
+function hover(e) {
+    this.style.backgroundColor = 'white'
+}
+
+function btnClicked(e) {
+    squareNumButton.classList.add('num-squares-clicked');
+    squareNumText.classList.add('num-squares-text-clicked');
+}
+
+blocks.forEach(block => block.addEventListener('mouseover', hover));
+squareNumButton.addEventListener('click', btnClicked);
+
